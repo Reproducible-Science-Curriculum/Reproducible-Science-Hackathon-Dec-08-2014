001eb05883b9bbf42b3dec3da784d09302209ef4
kcranston <karen.cranston@gmail.com>
2014-12-12 15:47:15 -0500

    fixed to grab text version

--- etherpads/get_etherpads.sh
+++ etherpads/get_etherpads.sh
@@ -2,11 +2,15 @@
 
 # Downloads all of the etherpads mentioned a given markdown document
 
-inputfile=$1
+https://etherpad.mozilla.org/ep/pad/export/vPGhZc6aBh/latest?format=txt
 
-for etherpad in `grep -ho 'etherpad.mozilla.org\/[a-zA-Z0-9]\{10\}' $1`
+inputfile=$1
+echo $inputfile
+
+for etherpad in `grep -ho 'https:\/\/etherpad.mozilla.org\/[a-zA-Z0-9]\{10\}' $1`
 do
 	echo $etherpad
 	padnumber=`echo $etherpad | sed 's|.*\([a-zA-Z0-9]\{10\}\)$|\1|'`
-	curl $etherpad > "etherpad_$padnumber.txt"
+	#echo "https://etherpad.mozilla.org/ep/pad/export/$padnumber/latest?format=txt"
+	curl "https://etherpad.mozilla.org/ep/pad/export/$padnumber/latest?format=txt" > "etherpad_$padnumber.txt"
 done
